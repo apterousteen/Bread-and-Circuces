7971bb5f50e5bef5041a3e26873a743c9e2a193b
Kotovshikov Sergei Vladimirovich <kotovshikov31415926@gmail.com>
2022-05-07 11:51:44 +0300

    added buttons

---
+++ Bread and Circuces/Assets/Scripts/ButtonsHandlers/AttackButtonHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AttackButtonHandler : MonoBehaviour
+{
+    public void HandleClick()
+    {
+        print("Attack clicked");
+    }
+}
---
+++ Bread and Circuces/Assets/Scripts/ButtonsHandlers/EndTurnButtonHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndTurnButtonHandler : MonoBehaviour
+{
+    public void HandleClick()
+    {
+        print("EndTurn clicked");
+    }
+}
