d39865ecd3bf16d308e6956cd28f3b99d2b6af91
apterousteen <ayzhan312@live.ru>
2022-05-17 01:13:20 +0500

    Main menu + choice menu
    - recovered layout from the 1st commit
    - added info change on side panel

---
+++ Bread and Circuces/Assets/Scripts/Ui/CharInfo.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharInfo : MonoBehaviour
+{
+    public string charName;
+    public int health;
+    public int moveDistance;
+    public int attackReachDistance;
+    public string info;
+}
