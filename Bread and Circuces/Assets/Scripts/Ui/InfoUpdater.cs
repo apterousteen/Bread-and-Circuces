b3636614b588f5433a8fe82c8959e154ad1d2334
apterousteen <ayzhan312@live.ru>
2022-05-18 22:10:21 +0500

    Fixed choice menu

---
+++ Bread and Circuces/Assets/Scripts/Ui/InfoUpdater.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class InfoUpdater : MonoBehaviour
+{
+    public CharInfoPanel CIP;
+    public GameObject Popup;
+
+    public void OnMouseDown()
+    {
+        Debug.Log(this.gameObject.tag + " Was Clicked");
+
+        var heroes = new List<CharInfo>
+        {
+            CIP.charInfoRet,
+            CIP.charInfoMurm,
+            CIP.charInfoSkis,
+            CIP.charInfoHoplo
+        };
+
+        foreach (var charInfo in heroes)
+        {
+            if (gameObject.tag != charInfo.charTag)
+            {
+                charInfo.cards.SetActive(false);
+                charInfo.charObj.GetComponent<Image>().color = new Color(1, 1, 1, 1);
+                charInfo.charObj.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 0);
+                continue;
+            }
+
+            MenuManager.chosen = charInfo.charObj;
+
+            charInfo.charObj.GetComponent<Image>().color = new Color(0, 1, 1, 1);
+            charInfo.charObj.transform.GetChild(0).GetComponent<Image>().color = new Color(1, 1, 1, 1);
+
+            CIP.charName.text = charInfo.charName;
+            CIP.health.text = charInfo.health.ToString();
+            CIP.moveDistance.text = charInfo.moveDistance.ToString();
+            CIP.attackReachDistance.text = charInfo.attackReachDistance.ToString();
+            CIP.info.text = charInfo.info;
+            charInfo.cards.SetActive(true);
+            CIP.cardPanel = charInfo.cards;
+        }
+    }
+}
