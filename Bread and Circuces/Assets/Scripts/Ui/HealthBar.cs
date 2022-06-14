1c554a62e6349fefab42fcd233e23acdebe04c79
apterousteen <ayzhan312@live.ru>
2022-06-15 00:04:43 +0500

    health bar colour change

--- Bread and Circuces/Assets/Scripts/Ui/HealthBar.cs
+++ Bread and Circuces/Assets/Scripts/Ui/HealthBar.cs
@@ -5,30 +5,11 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
-    public Slider Slider;
-    public Color Low;
-    public Color High;
-    public Vector3 Offset;
-
     public GameObject bar;
-
-    /*    public void SetHealth(float health, float maxHealth)
-        {
-            Slider.value = health;
-            Slider.maxValue = maxHealth;
-
-            Slider.fillRect.GetComponentInChildren<Image>().color = Color.Lerp(Low, High, Slider.normalizedValue);
-        }
-    */
 
     public void SetHealth(float health, float maxHealth)
     { 
         bar.transform.localScale = new Vector3((health / maxHealth), bar.transform.localScale.y, bar.transform.localScale.z);
+        bar.GetComponent<SpriteRenderer>().color = Color.Lerp(Color.black, new Color(0.7830189f, 0.1318247f, 0.04062834f, 1f), health / maxHealth);
     }
-
-    /*    private void Update()
-    {
-        Slider.transform.position = Camera.main.WorldToScreenPoint(transform.parent.position + Offset);
-    }
-    */
 }
