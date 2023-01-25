234ffc462430a5837eb4f14b2869115013cef017
bukinrr <Roman_bukin_01@mail.ru>
2023-01-25 13:31:31 +0500

    YandexTest

---
+++ Bread and Circuces/Assets/Scripts/Photon/PhotonTest/mainMenuPhotonButton.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class mainMenuPhotonButton : MonoBehaviour
+{
+    public void ButtonPlay()
+    {
+        SceneManager.LoadScene("PhotonTest");
+    }
+}
